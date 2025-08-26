518d9aa3fb628ca926f69caf6b0ec2463fde063a
𐌗˚🍁｡ 𝖣𝖾𝗏𝖥𝖾𝖽𝖾𝟣𝟥ㅤ⚽⋅ <fedexyz13@gmail.com>
2025-08-26 12:51:24 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,36 +1,58 @@
-const fs = require("fs");
-const chalk = require("chalk");
+import { watchFile, unwatchFile} from 'fs';
+import { fileURLToPath} from 'url';
+import chalk from 'chalk';
+import fs from 'fs';
+import moment from 'moment-timezone';
+import axios from 'axios';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
 
 // 🧑‍💻 Datos del bot
-global.owner = ["5491156178758"]; // ← Tu número aquí
+global.owner = [
+  ['5491156178758', 'DevFede', true],
+  ['573001533523', 'Brayan', true],
+];
+global.botNumber = '';
 global.sessionName = "FedeSession";
 global.version = "v2.4.0 | The-fede_IA";
 global.namebot = "The-fede_IA - DevFede13";
 global.author = "DevFede13 | The-fede_IA";
 
-// 💬 Mensajes personalizados
-global.mess = {
-  admin: "🍁 *Esta función está reservada para los administradores del grupo.*",
-  botAdmin: "🍁 *Para ejecutar esta función debo ser administrador.*",
-  owner: "🍁 *Solo mi creador puede usar este comando.*",
-  group: "🍁 *Esta función solo funciona en grupos.*",
-  private: "🍁 *Esta función solo funciona en mensajes privados.*",
-  wait: "🍁 *Espera un momento...*"
+// 🎨 Stickers
+global.packname = '🎀 The-fede_IA 🎋';
+global.author = `⟦ 📅 ${moment.tz('America/Los_Angeles').format('DD/MM/YY')} ⟧
+⟦ ⏰ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} ⟧
+⟦ 🧠 Powered by: DevFede ⟧`;
+
+// 📢 Canales y enlaces
+global.channel = 'https://whatsapp.com/channel/0029Vai28FR7dmea9gytQm3w';
+global.group = 'https://chat.whatsapp.com/IbADO35sBSC4G1FBTGbHIE?mode=ac_t';
+global.youtube = 'https://www.youtube.com/@ElCarlos.87';
+global.github = 'https://github.com/thecarlos19/black-clover-MD';
+
+// 🛠️ APIs
+global.APIKeys = {
+  'https://api.xteam.xyz': 'tu-key-xteam',
+  'https://api.lolhuman.xyz': 'tu-key-lol',
+};
+global.APIs = {
+  xteam: 'https://api.xteam.xyz',
+  lol: 'https://api.lolhuman.xyz',
 };
 
-// 🖼️ Imagen miniatura
-global.thumbnailUrl = "https://files.catbox.moe/r4w38m.jpg";
+// 🧩 Utilidades
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
+global.multiplier = 69;
+global.maxwarn = 3;
 
-// 📢 Canal oficial
-global.my = {
-  ch: "120363402097425674@newsletter" // ← Reemplaza con tu canal si lo deseas
-};
-
-// 🔁 Recarga automática del archivo
-let file = require.resolve(__filename);
-fs.watchFile(file, () => {
-  fs.unwatchFile(file);
-  console.log(chalk.yellowBright(`🔄 Archivo actualizado: '${__filename}'`));
-  delete require.cache[file];
-  require(file);
+// 🔁 Recarga automática
+let file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright("🔄 Config.js actualizado"));
+  import(`${file}?update=${Date.now()}`);
 });
